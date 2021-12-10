c58198e30dfd22d2316a4a3809eaa918c9de6569
Philip <philipokiokio@gmail.com>
2021-12-10 08:15:19 +0100

    Node API for file Upload

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+const multer = require("multer");
+const upload= multer({dest:'uploads/'});
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+app.post('/api/fileanalyse',upload.single('upfile'), (req,res,next)=>{
+  let file = req.file;
+  console.log(file);
+  let returnObj={
+    "name":file.originalname,
+    "type": file.mimetype,
+    "size":file.size
+  };
+  next(
+    res.json(returnObj)
+  );
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
